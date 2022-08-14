ccf55e0044638b151ef9690fa20c528cbd2f7101
Alex Frost <alexcc17@gmail.com>
2022-08-13 21:14:18 -0400

    Initial Commit

---
+++ EarthQuakes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthQuakes
+//
+//  Created by Alex Frost on 8/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
